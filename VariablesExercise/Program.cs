06d516e604e9944ccb81b86be6cba436763c532d
Greg Wanner <greg@tegogroup.com>
2024-02-15 10:36:17 -0500

    Add new variables and construct WriteLine using variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string clientName = "William";
+            int clientAge = 18;
+            char salaryCurrency = '$';
+            bool highSchoolGrad = true;
+            double gpa = 2.97;
+            decimal currentSalary = 13.25m;
+
+            Console.WriteLine($"{clientName} is {clientAge} years old.  His high school GPA was {gpa}.  His high school graduation status is {highSchoolGrad}.  He currently makes {salaryCurrency}{currentSalary} per hour working at a local store.");
         }
     }
 }
